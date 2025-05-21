0955457b2d67eeb5dab8d9571a5fd5dfc3db9162
sahlafauziah <sahlanurulfauziah18@gmail.com>
2025-05-21 20:07:51 +0700

    Add files via upload

---
+++ math.py
@@ -0,0 +1,14 @@
+def add(*numbers):
+    total = 0
+    for i in numbers:
+        total += i
+    return total
+
+def pow(num1,num2):
+    return num1 ** num2
+
+def multiply(*numbers):
+    total = 1
+    for i in numbers:
+        total *= i
+    return total
