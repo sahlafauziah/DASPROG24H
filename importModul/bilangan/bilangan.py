384039520b454a94f2b7767b6c28112c6fde8720
sahlafauziah <sahlanurulfauziah18@gmail.com>
2025-05-26 16:34:00 +0700

    Add files via upload

---
+++ importModul/bilangan/bilangan.py
@@ -0,0 +1,8 @@
+def desimalToBiner(n):
+    return f"{bin(n)}"
+
+def desimalToOktal(n):
+    return f"{oct(n)}"
+
+def desimalToHexadesimal(n):
+    return f"{hex(n)}"
---
+++ importModul/convert/convert.py
@@ -0,0 +1,5 @@
+def cmToMeter(cm):
+    return f"{cm / 100} Meter"
+
+def meterToCm(meter):
+    return f"{meter * 100} CM"
